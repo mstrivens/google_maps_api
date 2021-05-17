cd6652e1489a6b699df3aa944f3648d8c2409398
mstrivens <max.strivens@live.co.uk>
2021-05-17 14:35:39 +0100

    Google maps rendering and tabs implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import CurrentLocation from './Map';
+import Tabs from './components/Tabs';
+require('dotenv/config')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mapStyles = {
+  width: '60%',
+  height: '60%'
+};
+
+export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  }
+
+  onMarkerClick = (props, marker, e) =>
+  this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true,
+  })
+
+  onClose = props => {
+    if(this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+  render() {
+    return (
+      <Tabs>
+        <div label="cats-list">
+          List of cats
+        </div>
+        <div label="cats-map">
+          <Map
+            google={this.props.google}
+            zoom={14}
+            style={mapStyles}
+            initialCenter={
+              {
+                lat: 51.52542,
+                lng: -0.07055
+              }
+            }
+          >
+          <CurrentLocation
+            centerAroundCurrentLocation
+            google={this.props.google}
+          >
+            <Marker
+              onClick={this.onMarkerClick}
+              name={'Cat'}
+            />
+            <InfoWindow
+                marker={this.state.activeMarker}
+                visible={this.state.showingInfoWindow}
+                onClose={this.onClose}
+            >
+              <div>
+                <h4> {this.state.selectedPlace.name}</h4>
+              </div>
+            </InfoWindow>
+          </CurrentLocation>
+          </Map>
+        </div>
+      </Tabs>
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCmCWzBckFdaNiS2aTm8PnbVqUyIFVChrY'
+})(MapContainer)
